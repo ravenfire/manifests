f069febd76c7f4b5abb8f0c5115a52fc1ad82c6f
Michael Wilson <mwilson@electricjones.me>
2023-07-15 12:36:40 -0400

    manifest loader

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
-pub struct Fixtures {
+use std::{env, path::PathBuf};
 
+use common::traits::{DeserializationError, Tomlable};
+use peripheral::manifest::PeripheralManifest;
+
+fn get_crate_path() -> PathBuf {
+    env::var("CARGO_MANIFEST_DIR").map(PathBuf::from).unwrap()
+}
+
+pub struct Fixtures {}
+
+impl Fixtures {
+    pub fn peripheral(key: &str) -> Result<PeripheralManifest, DeserializationError> {
+        let path = get_crate_path().join("peripherals").join(key).join(".toml");
+        PeripheralManifest::from_toml_file(path)
+    }
 }
