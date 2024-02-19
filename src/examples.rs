49b5ceae1d70ef05b9ec1fe96bcadbf32a7bc375
Michael Wilson <mwilson@ravenfiretech.com>
2024-02-19 16:56:03 -0500

    update examples mocker

--- src/examples.rs
+++ src/examples.rs
@@ -1,84 +1,71 @@
-use common::data::serialization::Tomlable;
+use common::data::serialization::Jsonable;
 
 use crate::game::GameManifest;
 use crate::peripheral::PeripheralManifest;
 
 pub struct Peripheral {
-    toml: String,
+    json: String,
 }
 
 impl Peripheral {
     pub fn rf_dice_pad() -> Peripheral {
         Self {
-            toml: include_str!("../examples/peripherals/rf.dice_pad/rf.dice_pad.lock.toml")
-                .to_string(),
+            json: include_str!("../examples/peripherals/rf.dice_pad/rf.dice_pad.json").to_string(),
         }
     }
 
     pub fn rf_screen() -> Peripheral {
         Self {
-            toml: include_str!("../examples/peripherals/rf.screen/rf.screen.lock.toml").to_string(),
-        }
-    }
-
-    pub fn spellbinder_world_builder() -> Peripheral {
-        Self {
-            toml: include_str!(
-                "../examples/peripherals/spellbinder.world_builder/spellbinder.world_builder.lock.toml"
-            )
-                .to_string(),
+            json: include_str!("../examples/peripherals/rf.screen/rf.screen.json").to_string(),
         }
     }
 
     pub fn watertribe_card_reader() -> Peripheral {
         Self {
-            toml: include_str!(
-                "../examples/peripherals/watertribe.card_reader/watertribe.card_reader.lock.toml"
+            json: include_str!(
+                "../examples/peripherals/watertribe.card_reader/watertribe.card_reader.json"
             )
             .to_string(),
         }
     }
 
-    pub fn xl_card_reader() -> Peripheral {
-        Self {
-            toml: include_str!(
-                "../examples/peripherals/watertribe.xl_card_reader/watertribe.xl_card_reader.lock.toml"
-            )
-                .to_string(),
-        }
-    }
-
-    pub fn toml(&self) -> &str {
-        &self.toml
+    pub fn json(&self) -> &str {
+        &self.json
     }
 
     pub fn build(&self) -> PeripheralManifest {
-        PeripheralManifest::from_toml(self.toml()).expect("Failed to build PeripheralManifest")
+        PeripheralManifest::from_json(self.json()).expect("Failed to build PeripheralManifest")
     }
 }
 
 pub struct Game {
-    toml: String,
+    json: String,
 }
 
-impl crate::examples::Game {
-    pub fn spellbinder() -> crate::examples::Game {
+impl Game {
+    // pub fn spellbinder() -> crate::examples::Game {
+    //     Self {
+    //         json: include_str!("../examples/games/spellbinder/game.lock.json").to_string(),
+    //     }
+    // }
+    //
+    // pub fn tictactoe() -> crate::examples::Game {
+    //     Self {
+    //         json: include_str!("../examples/games/tictactoe/game.lock.json").to_string(),
+    //     }
+    // }
+
+    pub fn simple_battle() -> crate::examples::Game {
         Self {
-            toml: include_str!("../examples/games/spellbinder/game.lock.toml").to_string(),
+            json: include_str!("../examples/games/simple_battle/simple_battle.json").to_string(),
         }
     }
 
-    pub fn tictactoe() -> crate::examples::Game {
-        Self {
-            toml: include_str!("../examples/games/tictactoe/game.lock.toml").to_string(),
-        }
-    }
-
-    pub fn toml(&self) -> &str {
-        &self.toml
+    pub fn json(&self) -> &str {
+        &self.json
     }
 
     pub fn build(&self) -> GameManifest {
-        GameManifest::from_toml(self.toml()).expect("Failed to build GameManifest")
+        GameManifest::from_json(self.json()).expect("Failed to build GameManifest")
     }
 }
